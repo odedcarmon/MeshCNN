ac8d50bc14f6d513a15d0225c949c95ca2ca0075
odedcarmon <carmon.oded@gmail.com>
2020-12-09 13:36:25 +0200

    Create juggle_parameters.py

---
+++ scripts/human_seg/juggle_parameters.py
@@ -0,0 +1,23 @@
+from subprocess import run
+import sys
+def newTest(*param):
+    with open('./scripts/human_seg/test.sh','r') as infile, open('./scripts/human_seg/newTest.sh','w') as outfile:
+        for line in infile:
+            #if line.startswith("--ncf"):
+                #line = line[:6]+param[0]+line[-3:]
+            if line.startswith("--pool_res"):
+                line = line[:11]+param[1]+line[-3:]
+            outfile.write(line)
+
+if __name__=="__main__":
+    for i in range(4):
+        for j in range(4):
+            param=[" ".join([str(2**(i+1+k)) for k in range(2,6)])]
+            param.append("1800 "+("1560 ","")[i%2]+("1320 ","")[j%2]+("1080 ","")[(i//2)%2]+("840 ","")[(j//2)%2]+"600")
+            run(['chmod','+x','./scripts/human_seg/newTest.sh'],check=True)
+            newTest(*param)
+            #print("Testing with parametrs: --ncf "+param[0]+", --pool_res "+param[1]+":")
+            print("Testing with parametrs: --pool_res "+param[1]+":")
+            run(['bash','./scripts/human_seg/newTest.sh'],check=True)
+        
+    
