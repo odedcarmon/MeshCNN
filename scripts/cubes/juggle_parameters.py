1873cb3339cd1b9f8ec276b69a1d65f715562302
odedcarmon <carmon.oded@gmail.com>
2020-12-09 13:20:52 +0200

    Update juggle_parameters.py

--- scripts/cubes/juggle_parameters.py
+++ scripts/cubes/juggle_parameters.py
@@ -1,6 +1,9 @@
+
+
 from subprocess import run
+import sys
 def newTest(*param):
-    with open('test.sh','r') as infile, open('newTest.sh','w') as outfile:
+    with open('./scripts/cubes/test.sh','r') as infile, open('./scripts/cubes/newTest.sh','w') as outfile:
         for line in infile:
             if line.startswith("--ncf"):
                 line = line[:6]+param[0]+line[-3:]
@@ -13,9 +16,9 @@ if __name__=="__main__":
         for j in range(4):
             param=[" ".join([str(2**(i+1+k)) for k in range(2,6)])]
             param.append("600 "+("450 ","")[j%2]+("300 ","")[(j//2)%2]+"210")
-            run(['chmod','+x','newTest.sh'],check=True)
+            run(['chmod','+x','./scripts/cubes/newTest.sh'],check=True)
             newTest(*param)
             print("Testing with parametrs: --ncf "+param[0]+", --pool_res "+param[1]+":")
-            run(['bash','newTest.sh'],check=True)
+            run(['bash','./scripts/cubes/newTest.sh'],check=True)
         
     
