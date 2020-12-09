e4b43cdd78b06e31ebf909caeffe3250a123931a
odedcarmon <carmon.oded@gmail.com>
2020-12-09 13:20:17 +0200

    Revert "Update juggle_parameters.py"
    This reverts commit f6c049c526baef5cafdfa035a08a046a5053f075.

--- scripts/cubes/juggle_parameters.py
+++ scripts/cubes/juggle_parameters.py
@@ -1,7 +1,6 @@
 from subprocess import run
-import sys
 def newTest(*param):
-    with open('./scripts/'+sys.argv[1]+'/test.sh','r') as infile, open('./scripts/'+sys.argv[1]+'/newTest.sh','w') as outfile:
+    with open('test.sh','r') as infile, open('newTest.sh','w') as outfile:
         for line in infile:
             if line.startswith("--ncf"):
                 line = line[:6]+param[0]+line[-3:]
@@ -14,9 +13,9 @@ if __name__=="__main__":
         for j in range(4):
             param=[" ".join([str(2**(i+1+k)) for k in range(2,6)])]
             param.append("600 "+("450 ","")[j%2]+("300 ","")[(j//2)%2]+"210")
-            run(['chmod','+x','./scripts/'+sys.argv[1]+'/newTest.sh'],check=True)
+            run(['chmod','+x','newTest.sh'],check=True)
             newTest(*param)
             print("Testing with parametrs: --ncf "+param[0]+", --pool_res "+param[1]+":")
-            run(['bash','./scripts/'+sys.argv[1]+'/newTest.sh'],check=True)
+            run(['bash','newTest.sh'],check=True)
         
     
