46e46c0f5358da2aceb9eb17383bbaf13b5f3109
odedcarmon <carmon.oded@gmail.com>
2020-12-09 13:28:44 +0200

    Update juggle_parameters.py

--- scripts/cubes/juggle_parameters.py
+++ scripts/cubes/juggle_parameters.py
@@ -5,8 +5,8 @@ import sys
 def newTest(*param):
     with open('./scripts/cubes/test.sh','r') as infile, open('./scripts/cubes/newTest.sh','w') as outfile:
         for line in infile:
-            if line.startswith("--ncf"):
-                line = line[:6]+param[0]+line[-3:]
+            #if line.startswith("--ncf"):
+                #line = line[:6]+param[0]+line[-3:]
             if line.startswith("--pool_res"):
                 line = line[:11]+param[1]+line[-3:]
             outfile.write(line)
@@ -15,10 +15,11 @@ if __name__=="__main__":
     for i in range(4):
         for j in range(4):
             param=[" ".join([str(2**(i+1+k)) for k in range(2,6)])]
-            param.append("600 "+("450 ","")[j%2]+("300 ","")[(j//2)%2]+"210")
+            param.append("600 "+("525 ","")[i%2]+("450 ","")[j%2]+("375 ","")[(i//2)%2]+("300 ","")[(j//2)%2]+"210")
             run(['chmod','+x','./scripts/cubes/newTest.sh'],check=True)
             newTest(*param)
-            print("Testing with parametrs: --ncf "+param[0]+", --pool_res "+param[1]+":")
+            #print("Testing with parametrs: --ncf "+param[0]+", --pool_res "+param[1]+":")
+            print("Testing with parametrs: --pool_res "+param[1]+":")
             run(['bash','./scripts/cubes/newTest.sh'],check=True)
         
     
